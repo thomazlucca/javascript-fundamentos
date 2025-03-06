60816713154e2a56f4ca38c4b3d0d8a5cc13c827
Thomaz Lucca <thomazlucca@gmail.com>
2025-03-06 13:45:22 -0300

    add gitignore

--- 06-NodeJS/index.js
+++ 06-NodeJS/index.js
@@ -1,3 +1,33 @@
-console.log('olá mundo');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement).');
-console.log('Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+//função com async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch(erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+      }
+}
+
+//funcao assincrona utiliza o PROMISES com then()
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//       .readFile(caminhoDoArquivo, encoding)
+//       .then((texto) => console.log(chalk.green(texto)))
+//       .catch((erro) => trataErro(erro));
+// }
+
+
+pegaArquivo('./arquivos/texto.md');
